fa0efc2628cbf6f130b88555b67e37df919a3e1b
CLForde <fordecleveland4@gmail.com>
2022-05-23 02:34:16 -0400

    Lesson #4 - Request & Response
    Request object
    Response object

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   //   console.log('Server running');
-  console.log('Request made!');
-  res.write('Hello world');
+  // console.log(req.url, req.httpVersion, req.method, req.headers);
+
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<h1>Welcome</h1>');
+  res.write('<h2>Ninjas</h2>');
+  res.end();
 });
 
-server.listen('3000', 'localhost', () => {
+server.listen('3000', () => {
   console.log('Listening on port 3000');
 });
