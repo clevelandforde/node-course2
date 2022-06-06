4bf8661327dfa8b8b9ca7b32866a17048f23cf3e
CLForde <fordecleveland4@gmail.com>
2022-06-06 18:42:42 -0400

    Lesson #7 - View Engines
    View engines at a glance
    Ejs view engines
    Passing data into views
    Partials
    Adding css

--- App.js
+++ App.js
@@ -1,27 +1,42 @@
 const express = require('express');
 const { appendFile } = require('fs');
 const { STATUS_CODES } = require('http');
+
+// express app
 const App = express();
 
-App.get('/', (req, res) => {
-  //   res.send('Hello Express');
+// register view engine
+App.set('view engine', 'ejs');
 
-  res.sendFile('./views/index.html', { root: __dirname });
+App.get('/', (req, res) => {
+  const users = [
+    { firstName: 'Caius', lastName: 'Forde', idNo: 93747498 },
+    { firstName: 'Tia', lastName: 'Azore', idNo: 8947573 },
+    { firstName: 'Nicholas', lastName: 'Daw', idNo: 1240957 },
+  ];
+  res.render('index', { title: 'Home', users });
 });
 
 App.get('/about', (req, res) => {
-  res.sendFile('./views/about.html', { root: __dirname });
+  res.status(404).render('about', { title: 'About' });
 });
 
-App.get('/about-us', (req, res) => {
-  //   res.redirect('/about', '/views/about.html', { root: __dirname });
-  res.redirect('/about');
+App.get('/users/create', (req, res) => {
+  res.render('create', { title: 'Create new user' });
 });
 
+// // redirect
+// App.get('/about-us', (req, res) => {
+//   //   res.redirect('/about', '/views/about.html', { root: __dirname });
+//   res.redirect('/about');
+// });
+
+// 404 page
 App.use((req, res) => {
-  res.status(404).sendFile('/views/404.html', { root: __dirname });
+  res.render('404', { title: '404' });
 });
 
+// listen for requests
 App.listen(3000, () => {
   console.log('listening on port 3000 express');
 });
