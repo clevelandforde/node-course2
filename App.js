07cb7c6d066fa26ff8eb3ac7e8ab0cba8ab8e309
CLForde <fordecleveland4@gmail.com>
2022-06-20 22:28:06 -0400

    Lesson #10 - Get and Post Requests
    Request types
    Post request
    Route parameters

--- App.js
+++ App.js
@@ -10,11 +10,7 @@ const app = express();
 mongoose
   .connect(process.env.DB_URI)
   .then((conn) => {
-    app.listen(
-      3000,
-      console.log('Connected to database'),
-      console.log('Listening for requests on port 3000 express')
-    );
+    app.listen(3000, console.log('Connected to database'));
   })
   .catch((err) => console.log(err));
 
@@ -23,69 +19,12 @@ app.set('view engine', 'ejs');
 
 // middleware and static files
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 morgan('dev');
-
-// // mongoose and mongo sandbox routes
-// app.get('/add-user', (req, res) => {
-//   const user = new User({
-//     lastName: 'Forde',
-//     firstName: 'Leon',
-//     idNo: 123435,
-//     regimentNo: '54321',
-//     rank: 'civ',
-//   });
-//   user
-//     .save()
-//     .then((bio) => {
-//       res.send(bio);
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//     });
-// });
-
-// app.get('/single-user', (req, res) => {
-//   User.findById('62a90f43c92c3ed99dcc3a16')
-//     .then((singleRegistrant) => {
-//       res.send(singleRegistrant);
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//     });
-// });
-
-// app.get('/all-users', (req, res) => {
-//   User.find()
-//     .then((registrants) => {
-//       res.send(registrants);
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//     });
-// });
-
-// app.use((req, res, next) => {
-//   console.log('new request made');
-//   console.log('host: ', req.hostname);
-//   console.log('path: ', req.path);
-//   console.log('method: ', req.method);
-//   next();
-// });
-
-// app.use((req, res, nex) => {
-//   console.log('In the next middleware');
-//   nex();
-// });
 
 // basic routes
 app.get('/', (req, res) => {
   res.redirect('/users');
-  //   const users = [
-  //     { firstName: 'Caius', lastName: 'Forde', idNo: 93747498 },
-  //     { firstName: 'Tia', lastName: 'Azore', idNo: 8947573 },
-  //     { firstName: 'Nicholas', lastName: 'Daw', idNo: 1240957 },
-  //   ];
-  //   res.render('index', { title: 'Home', users });
 });
 
 app.get('/about', (req, res) => {
@@ -104,16 +43,32 @@ app.get('/users', (req, res) => {
     });
 });
 
+app.post('/users', (req, res) => {
+  const user = new User(req.body);
+  user
+    .save()
+    .then((result) => {
+      res.redirect('/users');
+    })
+    .catch((err) => console.log(err));
+});
+
 app.get('/users/create', (req, res) => {
   res.render('create', { title: 'Create new user' });
+});
+
+app.get('/users/:id', (req, res) => {
+  const id = req.params.id;
+  //   console.log('MyId', id);
+
+  User.findById(id)
+    .then((result) => {
+      res.render('details', { title: 'User details', user: result });
+    })
+    .catch((err) => console.log(err));
 });
 
 // 404 page
 app.use((req, res) => {
   res.render('404', { title: '404' });
 });
-
-// // listen for requests
-// app.listen(3000, () => {
-//   console.log('listening on port 3000 express');
-// });
